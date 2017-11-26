02fc3204f863fea26b16829b16e72cf8588b408f
Patrik Dudits <patrik@dudits.net>
2017-11-26 22:50:03 +0100

    Events of the gift application

---
+++ src/main/java/io/github/goodees/ese/example/gifts/event/GiftEvent.java
@@ -0,0 +1,12 @@
+package io.github.goodees.ese.example.gifts.event;
+
+import io.github.goodees.ese.core.Event;
+import io.github.goodees.ese.immutables.ImmutableEvent;
+
+/**
+ * Base class for events. 
+ * @author patrik
+ */
+public interface GiftEvent extends ImmutableEvent {
+    
+}
---
+++ src/main/java/io/github/goodees/ese/example/gifts/event/package-info.java
@@ -0,0 +1,5 @@
+@ImmutablesSupport
+package io.github.goodees.ese.example.gifts.event;
+
+import io.github.goodees.ese.immutables.ImmutablesSupport;
+
